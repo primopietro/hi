8f415e196493d68a8cb37a74b809ff8aacf79c3d
primopietro <Pietro.hyde@gmail.com>
2018-09-29 14:51:15 +0200

    push

--- experiments/island/src/js/app.js
+++ experiments/island/src/js/app.js
@@ -30,27 +30,7 @@ if(document.body) {
 
 
 function _init() {
-
-	//	LOADING ASSETS
-	if(assets.length > 0) {
-		document.body.classList.add('isLoading');
-
-		let loader = new AssetsLoader({
-			assets:assets
-		}).on('error', function (error) {
-			alert(error)
-			console.error(error);
-
-		}).on('progress', function (p) {
-			// console.log('Progress : ', p);
-			let loader = document.body.querySelector('.Loading-Bar');
-			if(loader) loader.style.width = (p * 100).toFixed(2) + '%';
-		}).on('complete', _onImageLoaded)
-		.start();	
-	} else {
-		_init3D();
-	}
-
+	_init3D();
 }
 
 
